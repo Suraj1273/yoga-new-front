8d4b3f17a4a056959ec2e77d8f0d5a12d6eac6fb
suraj <abcsuraj435@gmail>
2022-12-29 12:08:43 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { ViewCourseComponent } from './includes/view-course/view-course.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent},
+  {path:'course', component:ViewCourseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './includes/header/header.component';
 import { FooterComponent } from './includes/footer/footer.component';
 import { IndexComponent } from './index/index.component';
+import { ViewCourseComponent } from './includes/view-course/view-course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    IndexComponent
+    IndexComponent,
+    ViewCourseComponent
   ],
   imports: [
     BrowserModule,
