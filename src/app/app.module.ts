62b7720f8d1a7912380be855537a25f16bebdc0d
ayushrwt98 <ayush.rawat2123@gmail.com>
2023-01-03 17:16:05 +0530

    header dynamic and course page dynamic with slug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './includes/header/header.component';
 import { FooterComponent } from './includes/footer/footer.component';
 import { IndexComponent } from './index/index.component';
-import { ViewCourseComponent } from './includes/view-course/view-course.component';
 import { NgImageSliderModule } from 'ng-image-slider';
 import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
+import { CourseComponent } from './course/course.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     IndexComponent,
-    ViewCourseComponent
+    CourseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     NgImageSliderModule,
     NgxUsefulSwiperModule
   ],
