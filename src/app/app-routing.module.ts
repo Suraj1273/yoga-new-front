139ae2ea8207c4ab3342fee565086b29aa47da32
Sagar Tripathi <sgr.bzns@gmail.com>
2023-01-10 20:24:25 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { CourseComponent } from './course/course.component';
 import { TestStripeComponent } from './test-stripe/test-stripe.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent},
   // {path:'course', component:CourseComponent},
   {path:'course/:id', component:CourseComponent},
-  {path:'stripe', component:TestStripeComponent}
+  {path:'stripe', component:TestStripeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'blog', component:BlogComponent},
+  {path:'blog-detail', component:BlogDetailComponent}
 ];
 
 @NgModule({
